87f8dea79c9f51d7fef8a1a410d69bae5cd6f6fe
CharlotteGrossmann <charlotte.grossmann@hs-augsburg.de>
2021-06-22 01:45:14 +0200

    new build

--- client/src/components/logo/Logo.js
+++ client/src/components/logo/Logo.js
@@ -1,9 +1,9 @@
-import startingAnimation from '../../assets/start.gif';
+//import startingAnimation from '../../assets/start.gif';
 
 function Logo() {
 	return (
 		<div className='landing-logo'>
-			<img src={startingAnimation} alt='spiderweb' width='100%'></img>
+			{/* <img src={startingAnimation} alt='spiderweb' width='100%'></img> */}
 		</div>
 	);
 }
